944a375d99cc5ebc54e03f743f915be75099ef3f
Hong Sung <profsung@gmail.com>
2018-02-10 21:31:55 -0600

    status code corrected

--- app.js
+++ app.js
@@ -11,10 +11,9 @@ const Contact = require('./models/contact');
 app.get('/', (req, res) => {
 	Contact.find({}, (err, results) => {
 		if (err) {
-			res.render.status(505).send('<h1>Error</h1>');
-		} else {
-			res.render('index', {results, Contact});
+			return res.render.status(500).send('<h1>Error</h1>');
 		}
+		return res.render('index', {results, Contact});
 	});
 });
 
@@ -31,10 +30,9 @@ app.post('/add', (req, res) => {
 	});
 	newContact.save((err, results) => {
 		if (err) {
-			res.status(505).send('<h1>save() error</h1>', err);
-		} else {
-			res.redirect('/');
+			return res.status(500).send('<h1>save() error</h1>', err);
 		}
+		return res.redirect('/');
 	});
 });
 
@@ -55,20 +53,18 @@ app.post('/update', (req, res) => {
 	};
 	Contact.findOneAndUpdate(query, value, (err, results) => {
 		if (err) {
-			res.status(505).send('<h1>Update Error</h1>');
-		} else {
-			res.redirect('/');
+			return res.status(500).send('<h1>Update Error</h1>');
 		}
+		return res.redirect('/');
 	});
 });
 
 app.get('/remove', (req, res) => {
 	Contact.remove({_id: req.query._id}, (err, results) => {
 		if (err) {
-			res.status(505).send('<h1>Remove error</h1>');
-		} else {
-			res.redirect('/');
+			return res.status(500).send('<h1>Remove error</h1>');
 		}
+		return res.redirect('/');
 	});
 });
 
